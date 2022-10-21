fb1d26d47bde3519160ba25f667f0ca2bf577ae6
Ifan Efendi <115975303+Ifan-Efendi@users.noreply.github.com>
2022-10-22 00:25:05 +0700

    Add files via upload

---
+++ 2N.java
@@ -0,0 +1,9 @@
+public class ON2 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 5; x++) {
+            for (int y = 1; y <= x; y++) {
+                System.out.println("urutan x ke-" + x + " dan urutan y ke-" + y);
+            }
+        }
+    }
+}
