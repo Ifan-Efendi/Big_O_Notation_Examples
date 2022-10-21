5a21c441439227c7a05b329650a98444b51643fc
Ifan Efendi <115975303+Ifan-Efendi@users.noreply.github.com>
2022-10-22 00:24:02 +0700

    Delete 2N.java

--- 2N.java
+++
@@ -1,9 +0,0 @@
-public class 2N {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= Math.pow(2, n); i++) {
-            System.out.println("Data urutan ke" + i);
-        }
-
-    }
-}
